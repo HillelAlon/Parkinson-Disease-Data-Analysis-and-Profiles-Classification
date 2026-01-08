94eac4223f8976a8cabda53933e3207cb0f6fb2e
Tamar Kan <tamarush2612@gmail.com>
2026-01-08 14:02:13 +0200

    PCA table

---
+++ PCA_main_file.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import seaborn as sns
+
+df = pd.read_csv("/Users/tamarkan/PycharmProjects/PythonProject/Parkinson-Disease-Data-Analysis-and-Profiles-Classification/data/parkinsons_cleaned.csv")
+print(df.head())
+
+
