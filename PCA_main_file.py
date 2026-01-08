2c758005052c62d841e90f61b1fd4f38ad9dc4ed
Tamar Kan <tamarush2612@gmail.com>
2026-01-08 15:00:36 +0200

    PCA table Tamar and Alon created specific df for pca

--- PCA_main_file.py
+++ PCA_main_file.py
@@ -1,7 +1,20 @@
 import pandas as pd
-import seaborn as sns
 
 df = pd.read_csv("/Users/tamarkan/PycharmProjects/PythonProject/Parkinson-Disease-Data-Analysis-and-Profiles-Classification/data/parkinsons_cleaned.csv")
 print(df.head())
 
+# Keep only the sick patients in the data frame.
+df = df[df['Diagnosis'] != 0]
+print(df.head())
+print(df.info())
 
+# Keep only age, lifestyle and clinical measurements categories
+bool_cols_to_drop = [col for col in df.columns if len(set(df[col].unique())) < 5]
+df = df.drop(columns=bool_cols_to_drop)
+df = df.drop(columns=["PatientID","UPDRS", "MoCA", "FunctionalAssessment"]) #Remove categories: ID and assessments
+
+print(df.info())
+print(df.head(50))
+
+df.to_csv("parkinsons_lifestyle_clinical_for_PCA.csv",index=True)
+#/Users/tamarkan/PycharmProjects/PythonProject/Parkinson-Disease-Data-Analysis-and-Profiles-Classification/data
