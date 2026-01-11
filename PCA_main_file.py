d93eea93ce4f9e58ef1bfeb4c58828057b7f4f29
Alon Hillel <hillelalon22@gmail.com>
2026-01-11 19:15:07 +0200

    ... I changed the path inside the file PCA_main_file.py I added the pandas to the requirements.txt. I tried to make a code for transformation the values to z-score

--- PCA_main_file.py
+++ PCA_main_file.py
@@ -9,7 +9,7 @@ print(df.head())
 print(df.info())
 
 # Keep only age, lifestyle and clinical measurements categories
-bool_cols_to_drop = [col for col in df.columns if len(set(df[col].unique())) < 5]
+bool_cols_to_drop = [col for col in df.columns if len(set(df[col].unique())) < 5] #Remove boolean and categorical columns
 df = df.drop(columns=bool_cols_to_drop)
 df = df.drop(columns=["PatientID","UPDRS", "MoCA", "FunctionalAssessment"]) #Remove categories: ID and assessments
 
@@ -17,3 +17,13 @@ print(df.info())
 print(df.head(50))
 
 df.to_csv("data/parkinsons_lifestyle_clinical_for_PCA.csv",index=True)
+
+
+"""
+df1 = df
+print(df1.info())
+print(df1.head())
+for col in df.columns:
+    df1[f'{col} Z-Scores'] = (df[col] - df[col].mean()) / df[col].std()
+"""
+
